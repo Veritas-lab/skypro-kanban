db3040faee67eda7881fbe347eb894441543b59f
Veritas-lab <veritas91@list.ru>
2025-08-24 22:01:04 +0300

    добавлена страница авторизации

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Добавьте этот импорт
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {" "}
+      {/* Оберните App в BrowserRouter */}
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
