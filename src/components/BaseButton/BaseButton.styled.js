27a11eb9fbd9c5e2d7bd806e7a9a55375c416a08
Veritas-lab <veritas91@list.ru>
2025-11-30 19:21:40 +0300

    .

--- src/components/BaseButton/BaseButton.styled.js
+++ src/components/BaseButton/BaseButton.styled.js
@@ -12,19 +12,19 @@ export const SBaseButton = styled.button`
   font-weight: 500;
   &:hover {
     background-color: #33399b;
-    &:disabled {
-      background-color: rgba(148, 166, 190, 1);
-      cursor: not-allowed;
-    }
-    @media screen and (max-width: 495px) {
-      z-index: 3;
-      position: fixed;
-      left: 16px;
-      bottom: 30px;
-      top: auto;
-      width: calc(100vw - 32px);
-      height: 40px;
-      border-radius: 4px;
-    }
+  }
+  &:disabled {
+    background-color: rgba(148, 166, 190, 1);
+    cursor: not-allowed;
+  }
+  @media screen and (max-width: 495px) {
+    z-index: 3;
+    position: fixed;
+    left: 16px;
+    bottom: 30px;
+    top: auto;
+    width: calc(100vw - 32px);
+    height: 40px;
+    border-radius: 4px;
   }
 `;
